70137e6d7a9b817411e31b8942fbac15ff1afec6
Sau1244 <regec.hubert@gmail.com>
2024-12-25 16:11:21 +0100

    ex2 - uzupełnienie komentarzy

--- cpp/ex2/include/Animal.h
+++ cpp/ex2/include/Animal.h
@@ -14,15 +14,22 @@ public:
     //konstruktor z listą inicjalizacyjną dla name, age i sound
     Animal(const std::string& _name, int _age, const std::string& _sound);
 
+    //metoda wyświetlająca dane zwierząt
     void print() const;
+    //funkcja zwracająca wiek
     int getAge() const;
+    //funkcja zwracająca nazwe zwierzęcia
     const std::string& getName() const;
 
 };
 
+//metoda dodająca zwierzę do farmy
 void addAnimal(Animal*& animals, int& size, const Animal& newAnimal);
+//metoda usuwająca zwierzę z farmy
 void removeAnimal(Animal*& animals, int& size, const std::string& name);
+//komparator względem wieku
 bool compare(Animal& a, Animal& b);
+//metoda sortująca względem wieku
 void sortAnimals(Animal* animals, int size);
 
 #endif
